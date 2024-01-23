5d7f0d22f455fb21986822fbe5dccbc299984f82
Dylan <nunezdylam624@gmail.com>
2024-01-23 08:54:44 -0500

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,27 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'intro',
-    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
-  },
-  {
-    path: 'intro',
-    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
+    loadChildren: () => import('./intro/intro.module').then(m => m.IntroPageModule),
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
 })
-export class IntroPage implements OnInit {
+export class IntroPage {
+  username: string = 'dylan.nunez';
+  password: string = '1234';
 
-  constructor() { }
+  constructor(private navCtrl: NavController, private router: Router) {}
 
-  ngOnInit() {
+  login() {
+    // Lógica de inicio de sesión aquí
+
+    if (this.username === 'usuario' && this.password === 'contrasena') {
+      // Redirigir a la página home después del inicio de sesión exitoso
+      this.goToHome();
+    } else {
+      console.log('Error de inicio de sesión');
+    }
   }
 
+  goToHome() {
+    // Lógica para redirigir a la página 'home'
+    console.log("Go to home");
+    this.router.navigateByUrl('/home', { replaceUrl: true });
+  }
 }
